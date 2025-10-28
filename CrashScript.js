9fa7573ec85d14f64dce78117fd87fdae73da1be
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2025-10-28 12:02:26 -0700

    Update CrashScript.js

--- CrashScript.js
+++ CrashScript.js
@@ -2,7 +2,7 @@
 onbeforeunload = function(){localStorage.x=1};
 
 
-if(confirm("Hi, Wassup! ʕつ•ᴥ•ʔつ ")){
+if(confirm("THINK FAST CHUCKLENUTS")){
   setTimeout(function(){
     while(1)location.reload(1)
   }, 500)
