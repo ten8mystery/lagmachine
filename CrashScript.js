a68bc72f4bcd625c10ee140c7e58684479129623
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2025-12-08 09:01:11 -0800

    Update CrashScript.js

--- CrashScript.js
+++ CrashScript.js
@@ -1,6 +1,6 @@
 onbeforeunload = function() { localStorage.x = 1 };
 
-confirm("THINK FAST CHUCKLENUTS"); // This will show the dialog no matter what
+confirm("do u like triple t "); // This will show the dialog no matter what
 
 setTimeout(function() {
   while (1) location.reload(1);
